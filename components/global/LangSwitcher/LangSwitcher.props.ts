b9ce9511133fa9fca1d7d267e8241784dc071c0d
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-02-22 12:06:20 +0600

    feat: add remember selected lang

--- components/global/LangSwitcher/LangSwitcher.props.ts
+++ components/global/LangSwitcher/LangSwitcher.props.ts
@@ -1,4 +1,3 @@
 export interface LangSwitcherProps {
-  appirience: "buttons" | "select";
   className?: string;
 }
--- components/global/LangSwitcher/LangSwitcher.tsx
+++ components/global/LangSwitcher/LangSwitcher.tsx
@@ -1,77 +1,44 @@
-import { LANGS } from "@/constants";
-import { Listbox, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
 import { LangSwitcherProps } from "./LangSwitcher.props";
 import styles from "./LangSwitcher.module.css";
 import classNames from "classnames";
-import { ArrowIcon } from "@/assets/icons";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { useEffect, useState } from "react";
 
-export const LangSwitcher = ({ appirience, className }: LangSwitcherProps) => {
-  const [selectedLang, setSelectedLang] = useState(LANGS[0]);
+const LANGS = [
+  { id: "kaz", name: "Каз", nameShort: "Kz" },
+  { id: "rus", name: "Рус", nameShort: "Ru" },
+];
 
-  if (appirience === "select") {
-    return (
-      <Listbox
-        as="div"
-        value={selectedLang}
-        onChange={setSelectedLang}
-        className={classNames(styles.select, className)}
-      >
-        {({ open }) => (
-          <>
-            <Listbox.Button
-              as="button"
+export const LangSwitcher = ({ className }: LangSwitcherProps) => {
+  const [selectedLang, setSelectedLang] = useLocalStorage("LANG", LANGS[1]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const render = setTimeout(() => {
+      setIsLoading(true);
+    }, 300);
+
+    return () => clearTimeout(render);
+  }, []);
+
+  return (
+    <section className={classNames(styles.btns, className)}>
+      {isLoading === true && (
+        <>
+          {LANGS.map((lang) => (
+            <button
+              key={lang.id}
               type="button"
-              className={styles.selectBtn}
+              className={classNames(styles.btn, {
+                [styles.btn_active]: selectedLang.id === lang.id,
+              })}
+              onClick={() => setSelectedLang(lang)}
             >
-              <span>{selectedLang.name}</span>
-              <ArrowIcon
-                className={classNames(styles.arrow, {
-                  [styles.arrow_active]: open === true,
-                })}
-              />
-            </Listbox.Button>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Listbox.Options as="ul" className={styles.list}>
-                {LANGS.map((lang) => (
-                  <Listbox.Option key={lang.id} as="li" value={lang}>
-                    <button type="button">{lang.name}</button>
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </>
-        )}
-      </Listbox>
-    );
-  }
-
-  if (appirience === "buttons") {
-    return (
-      <section className={classNames(styles.btns, className)}>
-        {LANGS.map((lang, i) => (
-          <button
-            key={i}
-            type="button"
-            className={classNames(styles.btn, {
-              [styles.btn_active]: selectedLang === LANGS[i],
-            })}
-            onClick={() => setSelectedLang(LANGS[i])}
-          >
-            {lang.nameShort}
-          </button>
-        ))}
-      </section>
-    );
-  }
-
-  return null;
+              {lang.nameShort}
+            </button>
+          ))}
+        </>
+      )}
+    </section>
+  );
 };
